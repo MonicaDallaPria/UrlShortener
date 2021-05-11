f20f8494eab7ddddea30a9dcb1fd63c6ffe60220
mon.hailwood <mon.hailwood@gmail.com>
2021-05-11 07:55:54 +0100

    Working version

--- URLPayRoc/wwwroot/js/site.js
+++ URLPayRoc/wwwroot/js/site.js
@@ -1,18 +1,15 @@
 ﻿var submitBtn = document.querySelector("#submit");
 var urlInput = document.querySelector("#urlshort");
-var oldUrl = "";
+
 submitBtn.onclick = function (ev) {
     let url = urlInput.value;
     fetch("/", {
         method: "POST",
         body: JSON.stringify(url),
         headers: { 'Content-Type': 'application/json' }
-       
-    }).then(res => res.json())
-        .then(response => {
-            oldUrl = response;
-            console.log(oldUrl);
+    }).then(response => response.json())
+        .then(data => {
             document.querySelector("#resultText").href = url;
-            document.querySelector("#resultText").innerHTML = oldUrl;
-        }
-    )}
+            document.querySelector("#resultText").innerHTML = data;
+        })
+}
